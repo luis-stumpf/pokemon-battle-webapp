23bfc658d9a7d470b2706f16048a472d9ccac502
Luis St <trumpf25lp@gmail.com>
2023-07-22 13:28:50 +0200

    feat: display card with attacks

---
+++ frontend/src/lib/components/PokemonInfoCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let pokemon;
+</script>
+
+<div class="card w-1/3">
+    <header class="card-header">{pokemon.name}</header>
+    <section class="p-8">
+        <div class="flex items-center justify-center">
+            <img class="h-72 " alt="pokemon" src={pokemon.spriteFront}>
+        </div>
+        <div class="card">
+           <header class="card-header">Attacks</header>
+            <section class="p-2">
+                <div class="grid grid-cols-2 grid-rows-2 gap-x-5 gap-y-2">
+                    {#each pokemon.moveSet as move }
+                        <button type="button" class="btn variant-filled">{move}</button>
+                    {/each}
+                </div>
+            </section>
+        </div>
+    </section>
+</div>
--- frontend/src/routes/pokemon/[name]/+page.svelte
+++ frontend/src/routes/pokemon/[name]/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
     import type { PageServerLoad } from './$types'
+    import PokemonInfoCard from "$lib/components/PokemonInfoCard.svelte";
 
     export let data: PageServerLoad;
 </script>
 
-<div>
-    <img alt="pokemon" src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${data.name}.gif`}>
-    <img alt="pokemon" src={data.pokemon.spriteBack}>
-    <div>
-        {#each data.pokemon.moveSet as move }
-            <button type="button" class="btn variant-filled">{move}</button>
-        {/each}
-    </div>
+<div class="m-4">
+    <PokemonInfoCard pokemon={data.pokemon} />
 </div>
